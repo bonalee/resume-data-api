31060100ea19ad5df128d5da5e78137a31ccb080
Melissa Gonzalez <marvelissa@gmail.com>
2017-08-29 19:21:03 -0500

    Add experience show & index.  Add experience fake data to seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "students#index"
 
-  get '/experiences' => 'experiences/index'
-  post '/experiences' => 'experiences/create'
-  get '/experiences/:id' => 'experiences/show'
-  patch '/experiences/:id' => 'experiences/update'
-  delete '/experiences/:id' => 'experiences/destroy'
+  get '/experiences' => 'experiences#index'
+  post '/experiences' => 'experiences#create'
+  get '/experiences/:id' => 'experiences#show'
+  patch '/experiences/:id' => 'experiences#update'
+  delete '/experiences/:id' => 'experiences#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Experience.create(start_date: 3.years.ago, end_date: 2.years.ago, job_title: "Tightrope walker", company_title: "Cirque du Soleil", details: "Perform walking across circus 50 ft in the air.  Managed & surpassed customer fears & expectations.", student_id: 1)
+
+Experience.create(start_date: 25.months.ago, end_date: 6.months.ago, job_title: "Sword Swallower", company_title: "PT Barnum & Bailey Circuis", details: "Swallow Swords", student_id: 2)
+
+Experience.create(start_date: 20.months.ago, end_date: Time.now, job_title: "Ninja", company_title: "Teenage Mutant Turtles", details: "Fight crime.  Eat Pizza.", student_id: 3)
+
+Experience.create(start_date: 18.months.ago, end_date: Time.now, job_title: "Ice Cream Taster", company_title: "Dippin Dots", details: "Taste 200 samples of ice cream per day without getting brain freeze.", student_id: 4)
+
+Education.create(start_date: 5.years.ago, end_date: 4.years.ago, degree: "BS", university_name: "Univerity of Aweseome", details: "WJK;alhsoiuwerakvm,coja", student_id: 1)
+
